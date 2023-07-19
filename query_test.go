6f414d39eb517190500af8c72c2725d39509c9dd
Maarten van der Heijden <maarten.van.der.heijden@ing.com>
2023-07-19 16:14:01 +0200

    Improve test

--- query_test.go
+++ query_test.go
@@ -1,6 +1,7 @@
 package gormqonvert
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/ing-bank/gormtestutil"
 	"github.com/stretchr/testify/assert"
@@ -22,7 +23,7 @@ func TestGormQonvert_Initialize_TriggersConversionCorrectly(t *testing.T) {
 	defaultQuery := func(db *gorm.DB) *gorm.DB { return db }
 
 	tests := map[string]struct {
-		filter   map[string]any
+		filter   []map[string]any
 		existing []ObjectA
 		options  []Option
 		query    func(*gorm.DB) *gorm.DB
@@ -36,34 +37,35 @@ func TestGormQonvert_Initialize_TriggersConversionCorrectly(t *testing.T) {
 
 		// Check if everything still works
 		"simple where query": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"name": "jessica",
-			},
+			}},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 46}, {Name: "amy", Age: 35}},
 			expected: []ObjectA{{Name: "jessica", Age: 46}},
 		},
 		"more complex where query": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"name": "jessica",
 				"age":  53,
+			},
 			},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 53}, {Name: "jessica", Age: 20}},
 			expected: []ObjectA{{Name: "jessica", Age: 53}},
 		},
 		"multi-value where query": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"name": []string{"jessica", "amy"},
-			},
+			}},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 53}, {Name: "amy", Age: 20}},
 			expected: []ObjectA{{Name: "jessica", Age: 53}, {Name: "amy", Age: 20}},
 		},
 		"more complex multi-value where query": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"name": []string{"jessica", "amy"},
-				"age":  []int{53, 20},
+				"age":  []int{53, 20}},
 			},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 53}, {Name: "amy", Age: 20}},
@@ -72,69 +74,101 @@ func TestGormQonvert_Initialize_TriggersConversionCorrectly(t *testing.T) {
 
 		// On to the 'real' tests
 		"greater or equal to value": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": ">=30",
-			},
+			}},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 29}, {Name: "amy", Age: 30}, {Name: "boris", Age: 31}},
 			expected: []ObjectA{{Name: "amy", Age: 30}, {Name: "boris", Age: 31}},
 		},
 		"greater than value": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": ">30",
-			},
+			}},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 29}, {Name: "amy", Age: 30}, {Name: "boris", Age: 31}},
 			expected: []ObjectA{{Name: "boris", Age: 31}},
 		},
 		"less or equal to value": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": "<=30",
-			},
+			}},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 29}, {Name: "amy", Age: 30}, {Name: "boris", Age: 31}},
 			expected: []ObjectA{{Name: "jessica", Age: 29}, {Name: "amy", Age: 30}},
 		},
 		"less than value": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": "<30",
-			},
+			}},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 29}, {Name: "amy", Age: 30}, {Name: "boris", Age: 31}},
 			expected: []ObjectA{{Name: "jessica", Age: 29}},
 		},
 		"not equal to value": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": "!=30",
-			},
+			}},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 29}, {Name: "amy", Age: 30}, {Name: "boris", Age: 31}},
 			expected: []ObjectA{{Name: "jessica", Age: 29}, {Name: "boris", Age: 31}},
 		},
 
+		"between certain values": {
+			filter: []map[string]any{{
+				"age": []string{">30"},
+			}, {
+				"age": []string{"<35"},
+			}},
+			query:    defaultQuery,
+			existing: []ObjectA{{Name: "jessica", Age: 29}, {Name: "amy", Age: 30}, {Name: "boris", Age: 31}, {Name: "ahmed", Age: 33}, {Name: "jochem", Age: 36}},
+			expected: []ObjectA{{Name: "boris", Age: 31}, {Name: "ahmed", Age: 33}},
+		},
 		"not between certain values": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": []string{"<30", ">35"},
-			},
+			}},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 29}, {Name: "amy", Age: 30}, {Name: "boris", Age: 31}, {Name: "ahmed", Age: 33}, {Name: "jochem", Age: 36}},
 			expected: []ObjectA{{Name: "jessica", Age: 29}, {Name: "jochem", Age: 36}},
 		},
 		"not between certain values with another filter": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"name": []string{"boris"},
-				"age":  []string{">=30", "<=35"},
+				"age":  []string{">=30"},
+			}, {
+				"age": "<=35",
+			}},
+			query:    defaultQuery,
+			existing: []ObjectA{{Name: "boris", Age: 29}, {Name: "amy", Age: 30}, {Name: "boris", Age: 31}, {Name: "ahmed", Age: 33}, {Name: "josh", Age: 34}, {Name: "jochem", Age: 36}},
+			expected: []ObjectA{{Name: "boris", Age: 31}},
+		},
+		"not certain values": {
+			filter: []map[string]any{
+				{"age": "!=29"},
+				{"age": "!=30"},
+				{"age": "!=33"},
+				{"age": "!=34"},
+				{"age": "!=36"},
 			},
 			query:    defaultQuery,
 			existing: []ObjectA{{Name: "jessica", Age: 29}, {Name: "amy", Age: 30}, {Name: "boris", Age: 31}, {Name: "ahmed", Age: 33}, {Name: "josh", Age: 34}, {Name: "jochem", Age: 36}},
 			expected: []ObjectA{{Name: "boris", Age: 31}},
 		},
+		"after date": {
+			filter: []map[string]any{
+				{"date": fmt.Sprintf(">=%v", time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC))},
+			},
+			query:    defaultQuery,
+			existing: []ObjectA{{Name: "joris", Date: time.Date(2023, 1, 2, 0, 0, 0, 0, time.UTC)}, {Name: "dane", Date: time.Date(2022, 1, 2, 0, 0, 0, 0, time.UTC)}},
+			expected: []ObjectA{{Name: "joris", Date: time.Date(2023, 1, 2, 0, 0, 0, 0, time.UTC)}},
+		},
 
 		// With existing query
 		"greater or equal to value with existing query": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": ">=30",
-			},
+			}},
 			query: func(db *gorm.DB) *gorm.DB {
 				return db.Where("name = ?", "amy")
 			},
@@ -142,9 +176,9 @@ func TestGormQonvert_Initialize_TriggersConversionCorrectly(t *testing.T) {
 			expected: []ObjectA{{Name: "amy", Age: 30}},
 		},
 		"greater than value with existing query": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": ">30",
-			},
+			}},
 			query: func(db *gorm.DB) *gorm.DB {
 				return db.Where("name = ?", "boris")
 			},
@@ -152,9 +186,9 @@ func TestGormQonvert_Initialize_TriggersConversionCorrectly(t *testing.T) {
 			expected: []ObjectA{{Name: "boris", Age: 31}},
 		},
 		"less or equal to value with existing query": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": "<=30",
-			},
+			}},
 			query: func(db *gorm.DB) *gorm.DB {
 				return db.Where("name = ?", "jessica")
 			},
@@ -162,9 +196,9 @@ func TestGormQonvert_Initialize_TriggersConversionCorrectly(t *testing.T) {
 			expected: []ObjectA{{Name: "jessica", Age: 29}},
 		},
 		"less than value with existing query": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": "<30",
-			},
+			}},
 			query: func(db *gorm.DB) *gorm.DB {
 				return db.Where("name = ?", "jessica")
 			},
@@ -172,9 +206,9 @@ func TestGormQonvert_Initialize_TriggersConversionCorrectly(t *testing.T) {
 			expected: []ObjectA{{Name: "jessica", Age: 29}},
 		},
 		"not equal to value with existing query": {
-			filter: map[string]any{
+			filter: []map[string]any{{
 				"age": "!=30",
-			},
+			}},
 			query: func(db *gorm.DB) *gorm.DB {
 				return db.Where("name = ?", "boris")
 			},
@@ -210,11 +244,16 @@ func TestGormQonvert_Initialize_TriggersConversionCorrectly(t *testing.T) {
 			err := db.Use(plugin)
 
 			// Assert
-			assert.Nil(t, err)
+			assert.NoError(t, err)
 
 			var actual []ObjectA
-			err = testData.query(db).Where(testData.filter).Find(&actual).Error
-			assert.Nil(t, err)
+
+			query := testData.query(db)
+			for _, filter := range testData.filter {
+				query = query.Where(filter)
+			}
+
+			assert.NoError(t, query.Find(&actual).Error)
 
 			assert.Equal(t, testData.expected, actual)
 		})
@@ -305,11 +344,11 @@ func TestGormQonvert_Initialize_TriggersConversionCorrectlyWithSetting(t *testin
 			err := db.Use(plugin)
 
 			// Assert
-			assert.Nil(t, err)
+			assert.NoError(t, err)
 
 			var actual []ObjectB
 			err = db.Where(testData.filter).Find(&actual).Error
-			assert.Nil(t, err)
+			assert.NoError(t, err)
 
 			assert.Equal(t, testData.expected, actual)
 		})
